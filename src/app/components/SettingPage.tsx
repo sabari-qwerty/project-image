79b2ce70482a0bd1c7f60ebe8563b58ea3e48fb9
sabari-qwerty <120260499+sabari-qwerty@users.noreply.github.com>
2023-10-10 21:55:56 +0530

    update setting

--- src/app/components/SettingPage.tsx
+++ src/app/components/SettingPage.tsx
@@ -2,6 +2,7 @@
 import { FC, useEffect, useState } from "react";
 import { useUser } from "@auth0/nextjs-auth0/client";
 import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
 
 interface AdminPage {
   id: string;
@@ -14,7 +15,7 @@ interface AdminPage {
 export const SettingPage: FC = () => {
   const { user, error, isLoading } = useUser();
 
-  const [getUser, setGetUser] = useState<AdminPage>();
+  // const [getUser, setGetUser] = useState<AdminPage>();
 
   const [User, setUser] = useState(String(user?.name).split("@")[0]);
 
@@ -25,29 +26,22 @@ export const SettingPage: FC = () => {
   const [Image, setImage] = useState("");
 
   const GetUser = async () => {
-    if (user?.email) {
-      const data = await axios
-        .get(`/api/user/get/single?email=${user?.email}`)
-        .then((data) => data?.data?.data);
-      setUser(data?.name);
-      setEmail(data?.email);
-      setRole(data?.role);
-      setImage(data?.picture);
-    }
+    const data = await axios.get(`/api/user/get/single?email=${user?.email}`);
+
+    return data;
   };
 
-  useEffect(() => {
-    GetUser();
-
-    return () => undefined;
-  }, [user?.email]);
+  const useQuerGetUser = useQuery({
+    queryKey: ["getsingleuser"],
+    queryFn: () => GetUser(),
+  });
 
   return (
     <div className="w-4/5 mx-auto mt-10 flex flex-col space-y-8">
       <div className="flex justify-between">
         <div className="avatar">
           <div className="w-24 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
-            <img src={Image} alt="user" />
+            <img src={useQuerGetUser.data?.data?.data?.picture} alt="user" />
           </div>
         </div>
         <div className="flex flex-col justify-center items-center my-auto w-4/5 space-y-4 ">
@@ -58,7 +52,7 @@ export const SettingPage: FC = () => {
             name="name"
             type="text"
             id="name"
-            value={User}
+            value={useQuerGetUser.data?.data?.data?.name.split("@")[0]}
             onChange={(e) => setUser(e.target.value)}
             className="input input-bordered input-info w-full "
           />
@@ -73,7 +67,7 @@ export const SettingPage: FC = () => {
             name="email"
             type="email"
             id="email"
-            value={Email}
+            value={useQuerGetUser.data?.data?.data?.email}
             onChange={(e) => setEmail(e.target.value)}
             className="input input-bordered input-info w-full "
           />
@@ -86,7 +80,7 @@ export const SettingPage: FC = () => {
             name="role"
             type="role"
             id="role"
-            value={role}
+            value={useQuerGetUser.data?.data?.data?.role}
             onChange={(e) => setUser(e.target.value)}
             className="input input-bordered input-info w-full "
             disabled={true}
